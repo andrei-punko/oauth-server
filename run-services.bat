e981fcbe83a8d7eabe046479385d9d14ec9e5ae1
Andrei Punko <andd3dfx@gmail.com>
2020-12-14 19:58:13 +0300

    Fix script for starting services

--- run-services.bat
+++ run-services.bat
@@ -1,4 +1,4 @@
 
-start java -jar ./build/libs/entitlements-auth-0.0.1-SNAPSHOT.jar --server.port=9090 --auth.profile=ott
+start java -jar ./build/libs/oauth-server-0.0.1-SNAPSHOT.jar --server.port=9090 --auth.profile=ott
 
-start java -jar ./build/libs/entitlements-auth-0.0.1-SNAPSHOT.jar --server.port=9095 --auth.profile=mvpd
+start java -jar ./build/libs/oauth-server-0.0.1-SNAPSHOT.jar --server.port=9095 --auth.profile=mvpd
