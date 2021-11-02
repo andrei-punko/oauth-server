434e7fd0725ef3ee6fe04d2c0dd8fc70f29aba9a
Andrei Punko <andd3dfx@gmail.com>
2021-11-02 13:23:42 +0300

    Adjust links in README. Remove extra imports

--- src/test/java/by/andd3dfx/auth/AuthServerAppTest.java
+++ src/test/java/by/andd3dfx/auth/AuthServerAppTest.java
@@ -3,11 +3,8 @@ package by.andd3dfx.auth;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthServerAppTest {
 
